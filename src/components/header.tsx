9ec806d399a7687fb9c780d20ae6c83a48605a32
박성주 <psj001002@koreatech.ac.kr>
2026-06-09 23:13:24 +0900

    feat: header 구성

---
+++ src/components/header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <header className="w-full h-16 flex border border-b-black items-center justify-between px-4 bg-zinc-100">
+      <h1 className="text-xl font-bold text-gray-800">NameCraft</h1>
+    </header>
+  )
+}
