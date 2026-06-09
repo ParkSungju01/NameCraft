77f4557420372e6e1137ef0cae1bb2c0348acbc0
박성주 <psj001002@koreatech.ac.kr>
2026-06-09 23:25:46 +0900

    feat: add side navigation

---
+++ src/components/sideNav.tsx
@@ -0,0 +1,9 @@
+export default function SideNav() {
+  return (
+    <nav className="w-50 h-screen border-r border-gray-300 p-4 bg-zinc-100">
+      <ul className="space-y-4">
+        <li className="text-black">내 파일</li>
+      </ul>
+    </nav>
+  )
+}
