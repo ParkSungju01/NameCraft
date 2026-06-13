73a22fa21e592781bf43ea783de9696634223b1d
박성주 <psj001002@koreatech.ac.kr>
2026-06-13 23:34:53 +0900

    feat:react-icons 적용

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,36 @@
 import SideNav from "@/components/sideNav";
+import { SiTypescript, SiJavascript, SiReact } from "react-icons/si";
 
-const types = ["변수", "함수", "클래스", "파일", "상수"];
-const languages = ["TypeScript", "JavaScript", "React"];
-const styles = ["자동", "camelCase", "PascalCase", "snake_case"];
+interface DataItem {
+  name: string;
+  icon?: React.ReactNode;
+}
 
-const buttonVariants = (data: string[]) => {
+const types = [{ name: "변수"}, { name: "함수"}, { name: "클래스"}, { name: "파일"}, { name: "상수"}];
+const languages = 
+[
+  { name: "TypeScript",
+    icon: <SiTypescript color="#3178C6" className="w-4 h-4" />
+}, { name: "JavaScript",
+    icon: <SiJavascript color="#F7DF1E" className="w-4 h-4" />
+}, { name: "React",
+    icon: <SiReact color="#61DAFB" className="w-4 h-4" />
+}];
+const styles = [{ name: "자동"}, { name: "camelCase"}, { name: "PascalCase"}, { name: "snake_case"}];
+
+const buttonVariants = (data: DataItem[]) => {
   return (
     <div className="w-full flex overflow-hidden">
       {data.map((item) => (
-        <button key={item} className="border border-gray-300 w-full first:rounded-l-lg last:rounded-r-lg p-2">
-          {item}
+        <button key={item.name} className="flex items-center justify-center gap-1 border border-gray-300 w-full first:rounded-l-lg last:rounded-r-lg p-2">
+          {item.icon}
+          {item.name}
         </button>
       ))}
     </div>
   );
 };
-function ButtonGroup({ data, title }: { data: string[]; title:string }) {
+function ButtonGroup({ data, title }: { data: DataItem[]; title:string }) {
   return (
     <div className="w-full flex flex-col items-start gap-2">
       <span className="text-sm font-semibold text-black">{title}</span>
@@ -35,9 +50,9 @@ export default function Home() {
             <div className="w-full flex flex-col items-start gap-2">
               <span className="text-sm font-semibold text-black">프로젝트</span>
               <select className="w-full h-10 p-2 border rounded-lg border-gray-300">
-                <option value="project">프로젝트</option>
-                <option value="character">캐릭터</option>
-                <option value="company">회사</option>
+                <option value="project">선택</option>
+                <option value="character">미정</option>
+                <option value="company">미정</option>
               </select>
             </div>
             <div className="w-full flex flex-col h-40 items-start gap-2">
